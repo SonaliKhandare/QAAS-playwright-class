9329115903e73415171e922dbfd46b5c36c9c9ad
SonaliKhandare <sadaphule.sonali@gmail.com>
2026-05-10 12:01:57 +0530

    first commit

---
+++ 4April.js
@@ -0,0 +1,149 @@
+/**let day ="Monday"
+if (day=||"Monday"||"Tuesday"||"Wednesday"){
+    console.log("sad")
+}
+else if (day =|| "Thursday"||"Friday")
+{
+    console.log("happy")
+}
+else{
+    console.log("Super Happy")
+}
+    */
+   /** 
+let names =["Sonali","Bhavesh","Komal","Alisha"]
+for  (index=0;index<=3;index++)
+    
+     {
+        console.log("Welcome  " + names[index])
+        
+     }*/
+
+/*
+let nums=[1,10,3,20,4,32,23]
+let odd=0;
+let even=0;
+let sum=0;
+
+for  (let index=0; index < nums.length; index++)
+{
+
+     if (nums[index]%2==0)
+     {  
+        even =even+nums[index]
+
+        console.log(nums[index],"is even")
+       
+
+
+    }
+
+     else{
+         odd =odd+nums[index]
+         console.log(nums[index],"is odd")
+          
+          
+     }
+    }
+     console.log(odd)
+     console.log(even)
+
+/*
+let nums= [1,10,3,20,4,32,23]
+let sum=0;
+
+for  (let index=0; index < nums.length; index++)
+{
+    sum =sum+nums[index]
+}
+console.log (sum)
+*/
+
+/*
+let nums=[1,10,3,20,4,32,23]
+let odd=0;
+let even=0;
+let sum=0;
+
+for  (let index=0; index < nums.length; index++)
+{
+
+     if (nums[index]%2==0)
+     {  
+        even =even+nums[index]
+
+        console.log(nums[index],"is even")
+       
+
+
+    }
+
+     else{
+         odd =odd+nums[index]
+         console.log(nums[index],"is odd")
+          
+          
+     }
+    }
+     console.log(odd)
+     console.log(even)
+*/
+
+/*let status= [ 100, 200, 201, 301, 405, 304, 602, 302, 50 ]
+
+for  (let index=0; index < status.length; index++)
+{
+    let value =status[index];
+    if (value>=100 && value<200)
+    {
+        console.log("Information") 
+    }      
+     else if (value >=200 && value<300)
+     {
+        console.log("Success") 
+     }
+    else if (value >= 300 && value<400)
+    {
+        console.log("Redirection")   
+    }  
+    else if (value >= 400 && value<500)
+    {        console.log("Client error")
+}
+    else if (value >= 500 && value<600)
+    {
+        console.log("Server Error")
+    }
+    else
+        {(console.log("Not known"))}
+}*/
+
+/*
+for  (let index=0; index < status.length; index++)
+{
+    let value =status[index];
+    console.log(value/100);
+    if (value/100 === 1)
+    {
+        console.log("Information") 
+    }      
+     else if (value/ 100 === 2)
+     {
+        console.log("Success") 
+     }
+    else if (value/100 === 3)
+    {
+        console.log("Redirection")   
+    }  
+    else if (value/100 === 4)
+    {        console.log("Client error")
+}
+    else if (value / 100 === 5)
+    {
+        console.log("Server Error")
+    }
+    else
+        {
+            (console.log("Not known"))
+        }
+}
+*/
