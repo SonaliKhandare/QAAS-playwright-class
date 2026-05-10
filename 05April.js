1a72f0a100c25b6ab5590389aee22ede34bfa831
SonaliKhandare <sadaphule.sonali@gmail.com>
2026-05-10 12:10:03 +0530

    05April file added

---
+++ 05April.js
@@ -0,0 +1,83 @@
+//let name = ["Sonali", "Myra","Komal","Jiya"]
+//console.log(name)
+
+/** let  =["chocolates", "ice cream", "chips"]
+console.log(foods)
+foods.push ("Samosa")
+console.log(foods)
+console.log(foods.length)
+console.log(foods.include ("candy"))
+*/
+
+/**let users =["sonali", "Gulshan ", "komal"]
+users.push ("Bhavesh")
+console.log(users.includes("Bhavesh"))
+users.pop()
+users.push("Bhavesh")
+console.log(users)
+console.log(users.indexOf("sonali"))
+*/
+
+/*let user = {
+name : "Sonali",
+age : 50,
+state : "Pune",
+amIhungry : true,
+skills : ["JS", "Functional", "API"]
+}
+/**console.log(user.skills)
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+*/
+
+/**let name = {
+status : "success",
+message : "Login successfull",
+data : user
+}
+
+console.log( name)
+
+let status ="failure" 
+if( status =="success")
+    {
+    console.log("code ran successfully")
+
+}
+else
+    {
+        console.log("code gave an error")
+    }
+        */
+
+    
+
+/*let number =[10,3]
+let operation = "[["
+if( operation ==="+")
+    {
+    let addition = number[0] + number [1]
+    console.log(addition)
+
+}
+else if(operation ==="-")
+    {
+        let subtraction = number[0] - number [1]
+    console.log(subtraction)
+    }
+  else if(operation ==="*")
+    {
+        let mul = number[0] * number [1]
+    console.log(mul)
+    }    
+    else if(operation ==="/")
+    {
+        let div = number[0] / number [1]
+    console.log(div)
+    }    
+else {
+    console.log("Value incorrect")
+}*/
+
+    
